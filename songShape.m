0a18e25e7a833681ade4543532b53af9d03560b4
dakota002 <dakota.d@live.com>
2016-03-25 12:04:20 -0400

    Added files via upload

---
+++ songShape.m
@@ -0,0 +1,17 @@
+%Makes points based on song using uniForm function
+function out = songShape(Song)
+y = audioread(Song);
+n=floor(length(y)/6);
+y1=y(1:n);
+y2=y(n+1:2*n);
+y3=y(2*n+1:3*n);
+y4=y(3*n+1:4*n);
+y5=y(4*n+1:5*n);
+y6=y(5*n+1:6*n);
+Y=[y1;y2;y3;y4;y5;y6];
+YT=Y.';
+Z=(Y*YT)*1/(n);
+e=eig(Z)*(2^8);
+diary songShape.txt
+out=uniForm(e(1),e(2),e(3),e(4),e(5),e(6))
+diary off
---
+++ uniForm.m
@@ -0,0 +1,16 @@
+function pointSets = uniForm(a,b,m,n_1,n_2,n_3)
+theta=(-pi:.05:pi);
+phi=(-pi/2:.05:pi/2);
+pointSets=[];
+for i=1:length(theta)
+    for j=1:length(phi)
+        t1=(m*theta(i)/4);
+        t2=(m*phi(j)/4);
+        r1=((abs(cos(t1)/a).^n_2)+((abs(sin(t1)/b).^n_3))).^(-1/n_1);
+        r2=((abs(cos(t2)/a).^n_2)+((abs(sin(t2)/b).^n_3))).^(-1/n_1);
+        x=r1*cos(theta(i))*r2*cos(phi(j));
+        y=r1*sin(theta(i))*r2*cos(phi(j));
+        z=r2*sin(phi(j));
+        pointSets=[pointSets; x y z];
+    end
+end

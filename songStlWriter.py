4c99bcb8e59fe3c04e836db8c8721640218ae044
dakota002 <dakota.d@live.com>
2016-05-13 00:01:09 -0400

    Update songStlWriter.py

--- songStlWriter.py
+++ songStlWriter.py
@@ -12,30 +12,52 @@ while True:
 page=open(f,'r+')
 lines=page.readlines()
 #first we find and get the scale factor for each number
+#Formatting changes depending on start line. So we have different conditions
+#for starting on 3 or 5
 if len(lines[3])<20:
     mant=float(lines[3][3:6])
     power=float(lines[3][8:10])
     scale=mant*10**power
     start=5
+    #then we make a list for the points
+    points=[]
+    for i in range(start,len(lines)-1):
+        tempLst=[]
+        for j in range(3):
+            ten=j*10
+            tPt=scale*float(lines[i][ten+4:ten+10])
+            sign=lines[i][ten+3]
+            if sign=='-':
+                tPt=(-1*tPt)
+            tempLst.append(tPt)
+        points.append(tempLst)
+    #here marks the end of the points list
+
 else:
     scale=1
     start = 3
-print(scale)#scale is good
-print(lines[start])
-#then we make a list for the points
-points=[]
-for i in range(start,len(lines)-1):
-    tempLst=[]
-    for j in range(3):
-        ten=j*10
-        tPt=scale*float(lines[i][ten+4:ten+10])
-        sign=lines[i][ten+3]
-        if sign=='-':
-            tPt=(-1*tPt)
-        tempLst.append(tPt)
-    points.append(tempLst)
-#here marks the end of the points list
-    
+    points=[]
+    #then we make list for points
+    for i in range(start,len(lines)-1):
+        L4=range(1,5)
+        L4.reverse()
+        curLine=lines[i]
+        for j in L4:
+            curLine=curLine.replace(j*' ',',')
+        commas=[]
+        for j in range(len(curLine)):
+            if curLine[j]==',':
+                commas.append(j)
+        tPt1=curLine[commas[0]+1:commas[1]]
+        tPt2=curLine[commas[1]+1:commas[2]]
+        tPt3=curLine[commas[2]+1:len(curLine)-1]
+        points.append([tPt1,tPt2,tPt3])
+
+
+#Test lines for confirming scale and lines
+'''print(scale)
+print(lines[start])'''
+
 #now we need to connect the dots into triangles
 stMat=[]
 for i in range(len(points)):
@@ -83,13 +105,23 @@ for i in range(len(points)):
         stMat.append(uTrip)
     if lTrip!=[]:
         stMat.append(lTrip)
-     
-print(stMat[0][0])
+
+
+#Test lines to confirm that points and stMat are not empty
+'''print(stMat[0][0])
 print(stMat[len(stMat)-1])
 print(len(points))
-print(points[len(points)-1])
+print(points[len(points)-1])'''
 page.close()
 
+#Writes a text file with the triangles listed as [[x,y,z],[x,y,z],[x,y,z]].
+#Used for debugging mainly
+'''with open('stMat.txt','w+') as file:
+    for i in stMat:
+        file.write(str(i)+'\n')'''
+
+
+#Writes the stl file from the stMat, however, leaves an open hole in the top
 with open(f+'.stl','w+') as file:
     file.write('\nsolid Default')
     for i in range(len(stMat)):
@@ -98,5 +130,10 @@ with open(f+'.stl','w+') as file:
             file.write('      vertex '+str(stMat[i][j][0])+' '+str(stMat[i][j][1])+' '+str(stMat[i][j][2]))
             file.write('\n')
         file.write('    endloop\n  endfacet')
+    for i in range(125):
+        file.write('\n  facet normal 0.000000e+00 0.000000e+00 -1.000000e+00 \n    outer loop \n')
+        file.write('      vertex '+str(points[0][0])+' '+str(points[0][1])+' '+str(points[0][2])+'\n')
+        file.write('      vertex '+str(points[i*63+1][0])+' '+str(points[i*63+1][1])+' '+str(points[i*63+1][2])+'\n')
+        file.write('      vertex '+str(points[(i+1)*63+1][0])+' '+str(points[(i+1)*63+1][1])+' '+str(points[(i+1)*63+1][2])+'\n')
+        file.write('    endloop\n  endfacet')
     file.write('\nendsolid Default')
-    

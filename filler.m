b051f19f8633b69277b515e2ac22e011a48bd9d6
Dakota Dutko <dakota.d@8th-sphere.com>
2016-09-05 15:34:09 -0400

    New function to make missing point

---
+++ filler.m
@@ -0,0 +1,11 @@
+function fillPoint = filler(a,b,m,n_1,n_2,n_3)
+theta=(-pi:.05:pi);
+phi=(-pi/2:.05:pi/2);
+t1=(m*pi/8);
+t2=(m*pi/8);
+r1=((abs(cos(t1)/a).^n_2)+((abs(sin(t1)/b).^n_3))).^(-1/n_1);
+r2=((abs(cos(t2)/a).^n_2)+((abs(sin(t2)/b).^n_3))).^(-1/n_1);
+x=r1*cos(theta(i))*r2*cos(phi(j));
+y=r1*sin(theta(i))*r2*cos(phi(j));
+z=r2*sin(phi(j));
+fillPoint=[x y z];
